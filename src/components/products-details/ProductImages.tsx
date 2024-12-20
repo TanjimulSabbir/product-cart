375b2f7033f9e94b88b9535d29b5672637635e1c
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-12-20 22:31:42 +0600

    image preview solved

--- src/components/products-details/ProductImages.tsx
+++ src/components/products-details/ProductImages.tsx
@@ -1,27 +1,28 @@
 import { Image } from "antd";
 import { useState } from "react";
-import { products, Product } from "../../utils/prodcuts.tsx"; 
+import { products, Product } from "../../utils/prodcuts.tsx";
 
 interface ProductImagesProps {
-  selectedColor: string; 
+    selectedColor: string;
 }
 
 export default function ProductImages({ selectedColor }: ProductImagesProps) {
-  const product: Product = products[0]; 
-  const [current, setCurrent] = useState(0);
+    const product: Product = products[0];
+    const [current, setCurrent] = useState(0);
 
-  const imageList = product.images[selectedColor];
+    const imageList = product.images[selectedColor];
 
-  return (
-    <main>
-      <Image.PreviewGroup
-        preview={{
-          toolbarRender: () => null,
-          onChange: (index) => setCurrent(index),
-        }}
-      >
-        <Image src={imageList} width={200} />
-      </Image.PreviewGroup>
-    </main>
-  );
+    return (
+        <main>
+            <Image.PreviewGroup
+                items={product.imageItems}
+                preview={{
+                    toolbarRender: () => null,
+                    onChange: (index) => setCurrent(index),
+                }}
+            >
+                <Image src={imageList} width={200} />
+            </Image.PreviewGroup>
+        </main>
+    );
 }
--- src/utils/prodcuts.tsx
+++ src/utils/prodcuts.tsx
@@ -4,47 +4,54 @@ import cyanWatch from "../assets/product-details/cyan-product.png";
 import blackWatch from "../assets/product-details/black-product.png";
 
 export interface Size {
-  label: string;
-  price: number;
+    label: string;
+    price: number;
 }
 
 export interface Product {
-  id: number;
-  name: string;
-  description: string;
-  model: string;
-  type: string;
-  basePrice: number;
-  discountPrice: number;
-  colors: string[];
-  sizes: Size[];
-  images: Record<string, string>;
+    id: number;
+    name: string;
+    description: string;
+    model: string;
+    type: string;
+    basePrice: number;
+    discountPrice: number;
+    colors: string[];
+    sizes: Size[];
+    images: Record<string, string>;
+    imageItems: string[]
 }
 
 export type Color = "pink" | "black" | "blue" | "cyan";
 
 export const products: Product[] = [
-  {
-    id: 1,
-    name: "Classy Modern Smart Watch",
-    description:
-      "I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teaching.",
-    model: "Forerunner 290XT",
-    type: "Watch",
-    basePrice: 79,
-    discountPrice: 99,
-    colors: ["pink", "black", "blue", "cyan"],
-    sizes: [
-      { label: "S", price: 79 },
-      { label: "M", price: 89 },
-      { label: "L", price: 99 },
-      { label: "XL", price: 109 },
-    ],
-    images: {
-      pink: pinkWatch,
-      black: blackWatch,
-      blue: blueWatch,
-      cyan: cyanWatch,
+    {
+        id: 1,
+        name: "Classy Modern Smart Watch",
+        description:
+            "I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teaching.",
+        model: "Forerunner 290XT",
+        type: "Watch",
+        basePrice: 79,
+        discountPrice: 99,
+        colors: ["pink", "black", "blue", "cyan"],
+        sizes: [
+            { label: "S", price: 79 },
+            { label: "M", price: 89 },
+            { label: "L", price: 99 },
+            { label: "XL", price: 109 },
+        ],
+        images: {
+            pink: pinkWatch,
+            black: blackWatch,
+            blue: blueWatch,
+            cyan: cyanWatch,
+        },
+        imageItems: [
+            pinkWatch,
+            blackWatch,
+            blueWatch,
+            cyanWatch,
+        ],
     },
-  },
 ];
